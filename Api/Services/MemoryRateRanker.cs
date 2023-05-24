f6c1bdf5c45ce1b0cafc65d0647c8e713aec3b82
Phil <phil.van.li@gmail.com>
2023-05-24 23:34:15 +0800

    implement IRateRanker

--- Api/Services/MemoryRateRanker.cs
+++ Api/Services/MemoryRateRanker.cs
@@ -1,10 +1,11 @@
-﻿namespace Api.Services;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Api.Services;
 
 public class MemoryRateRanker: IRateRanker
 {
     public string RankerName => nameof(MemoryRateRanker);
-
-    public SortedDictionary<double, double> ranges = new()
+    private static readonly SortedDictionary<double, double> ranges = new()
     {
         { 200_000_000, 0.045d},
         { 50_000_000d, 0.04d},
@@ -14,6 +15,11 @@ public class MemoryRateRanker: IRateRanker
     
     public double GetRate(double deposit)
     {
+        foreach (var key in ranges.Keys)
+        {
+            if (deposit >= key) return ranges[key];
+        }
+
         return 0d;
     }
 }
